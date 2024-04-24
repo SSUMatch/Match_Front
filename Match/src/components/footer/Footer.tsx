9ed152fd9e8335dc8e0e26ad649444f503a5d74c
onejuneee <onejuneee721@gmail.com>
2024-04-25 07:51:13 +0900

    feat: 헤더, 푸터 구현

---
+++ Match/src/components/footer/Footer.tsx
@@ -0,0 +1,19 @@
+import * as F from "./Styles.tsx";
+// import logo from "../../assets/icons/header_logo.svg";
+
+function Footer() {
+  return (
+    <F.Footer>
+      <F.Box>
+        {/* <F.LogoWrapper src={logo} alt="logo" /> */}
+        <F.LogoText>Match</F.LogoText>
+      </F.Box>
+      <F.TextWrapper>
+        <F.Text>개인정보 처리 방침</F.Text>
+        <F.Line />
+        <F.Text>TEAM Match</F.Text>
+      </F.TextWrapper>
+    </F.Footer>
+  );
+}
+export default Footer;
---
+++ Match/src/components/header/Header.tsx
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import * as H from "./Styles";
+import { LoggedOut } from "@/components/auth/LoggedOut.tsx";
+
+function Header() {
+  const navigate = useNavigate();
+  return (
+    <H.Header>
+      <H.RightContainer>
+        <H.RowContainer>
+          <H.LogoWrap>
+            {/* <HeaderIcon onClick={() => navigate("/")} /> */}
+          </H.LogoWrap>
+          <H.Gap>
+            <LoggedOut />
+            {/* {LoggedIn ? ( */}
+            {/*   <div> */}
+            {/*     <H.Button onClick={MypageHandler}>마이페이지</H.Button> */}
+            {/*     <H.Button onClick={handleLogout}>로그아웃</H.Button> */}
+            {/*   </div> */}
+            {/* ) : ( */}
+            {/*   <LoggedOut /> */}
+            {/* )} */}
+          </H.Gap>
+        </H.RowContainer>
+      </H.RightContainer>
+
+      <H.BtnWrap>
+        <H.HeaderBtn onClick={() => navigate("/")}>홈</H.HeaderBtn>
+        <H.HeaderBtn onClick={() => navigate("/favorites")}>
+          즐겨찾기
+        </H.HeaderBtn>
+        <H.HeaderBtn onClick={() => navigate("/archive")}>
+          기록 저장소
+        </H.HeaderBtn>
+        <H.HeaderBtn onClick={() => navigate("/ranking")}>랭킹</H.HeaderBtn>
+        <H.HeaderBtn onClick={() => navigate("/nearby")}>
+          주변 구장 찾기
+        </H.HeaderBtn>
+      </H.BtnWrap>
+    </H.Header>
+  );
+}
+
+export default Header;
