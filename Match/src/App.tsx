c0965f35e0d45bad974d6ee17f8f0fcd23832131
onejuneee <onejuneee721@gmail.com>
2024-04-25 07:53:18 +0900

    feat: 라우팅 설정, 화면 랜더링 설정

--- Match/src/App.tsx
+++ Match/src/App.tsx
@@ -1,40 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../vite.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+import "./App.css";
+import Home from "@/pages/home/Home.tsx";
+import Favorites from "@/pages/favorites/Favorites.tsx";
+import Archive from "@/pages/archive/Archive.tsx";
+import Ranking from "@/pages/ranking/Ranking.tsx";
+import NearBy from "@/pages/nearBy/NearBy.tsx";
+// import { AuthProvider } from "./components/auth/AuthProvider.tsx";
+
+const loading = <div>화면 로딩중...</div>;
+const DefaultLayout = React.lazy(() => import("./layout/DefaultLayout"));
+// const Home = React.lazy(() => import("./pages/home/Home"));
+// const Favorites = React.lazy(() => import("./pages/favorites/Favorites"));
+// const Archive = React.lazy(() => import("./pages/archive/Archive"));
+// const Ranking = React.lazy(() => import("./pages/ranking/Ranking"));
+// const NearBy = React.lazy(() => import("./pages/nearBy/NearBy"));
+// const Login = React.lazy(() => import("./pages/Login"));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Suspense fallback={loading}>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<Home />} />
+            <Route path="favorites" element={<Favorites />} />
+            <Route path="archive" element={<Archive />} />
+            <Route path="ranking" element={<Ranking />} />
+            <Route path="nearby" element={<NearBy />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
---
+++ Match/src/routes.ts
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Home = React.lazy(() => import("@/pages/home/Home.tsx"));
+// const Login = React.lazy(() => import('./pages/Login'));
+// const SignIn = React.lazy(() => import('./pages/SignIn'));
+const Favorites = React.lazy(() => import("@/pages/favorites/Favorites.tsx"));
+const Archive = React.lazy(() => import("@/pages/archive/Archive.tsx"));
+const Ranking = React.lazy(() => import("./pages/ranking/Ranking.tsx"));
+const NearBy = React.lazy(() => import("./pages/nearBy/NearBy.tsx"));
+
+const routes = [
+  { path: "/", element: Home },
+  // { path: '/login', element: Login },
+  // { path: '/signIn', element: SignIn },
+  { path: "/favorites", element: Favorites },
+  { path: "/archive", element: Archive },
+  { path: "/ranking", element: Ranking },
+  { path: "/nearby", element: NearBy },
+];
+
+export default routes;
