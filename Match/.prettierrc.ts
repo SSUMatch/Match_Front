7e41f2acee773dd5eed436ff134455d47df0a2d3
onejuneee <onejuneee721@gmail.com>
2024-04-24 23:34:39 +0900

    init : prettier 설정

---
+++ Match/.prettierrc.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true, // 세미콜론을 항상 사용
+  tabWidth: 2, // 탭 너비는 2 공백
+  printWidth: 80, // 한 줄의 최대 길이는 80 문자
+  singleQuote: true, // 문자열에 싱글 쿼트 사용
+  trailingComma: 'all', // 모든 가능한 곳에 후행 콤마 사용
+  jsxSingleQuote: true, // JSX에 싱글 쿼트 사용
+  useTabs: false, // 탭 대신 스페이스 사용
+  endOfLine: 'auto', // 개행 문자는 시스템에 맞춤
+  arrowParens: 'avoid', // 화살표 함수의 매개변수가 하나일 때 괄호 생략
+  bracketSpacing: false, // 객체 리터럴의 괄호 사이에 공백 사용하지 않음
+  htmlWhitespaceSensitivity: 'css' // HTML 파일의 공백 감도는 CSS 규칙을 따름
+};
