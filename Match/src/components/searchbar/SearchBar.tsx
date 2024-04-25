6251e16146d6ebfed72c0a55e37ec6b7f63c6fa3
onejuneee <onejuneee721@gmail.com>
2024-04-26 08:07:50 +0900

    feat: 검색창 구현

---
+++ Match/src/components/searchbar/SearchBar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import * as S from "./Styles";
+
+function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Searching for:", searchTerm);
+  };
+
+  return (
+    <S.SearchBarContainer>
+      <form
+        onSubmit={handleSearchSubmit}
+        className="flex items-center space-x-2 shadow-md rounded-full p-2 w-full max-w-md"
+      >
+        {/* <S.IconDiv>/!* <S.SearchIcon /> *!/</S.IconDiv> */}
+        <S.Input
+          type="text"
+          placeholder="지역, 팀 이름으로 검색"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </form>
+    </S.SearchBarContainer>
+  );
+}
+
+export default SearchBar;
---
+++ Match/src/components/searchbar/Styles.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+import Color from "@/styles/Color.ts";
+import Font from "@/styles/Font.ts";
+// import { ReactComponent as SearchIconSVG } from "../../assets/svg/search.svg";
+
+export const Container = styled.div`
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1440px;
+`;
+
+export const SearchBarContainer = styled.div`
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  flex-direction: row;
+  background-color: #fff;
+  padding-left: 3rem;
+  justify-content: flex-start;
+  align-items: center;
+  color: ${Color.MainColor};
+  background: rgba(204, 204, 204, 0.2);
+  border-radius: 2.5rem;
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  background: transparent;
+  border: none;
+  outline: none;
+  font-size: ${Font.SIZE.BODY3};
+  font-weight: ${Font.WEIGHT.REGULAR};
+  height: 3rem;
+  &::placeholder {
+    color: ${Color.MainLight};
+  }
+`;
+
+export const IconDiv = styled.div`
+  width: 24px;
+  height: 24px;
+  margin-right: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// export const SearchIcon = styled(SearchIconSVG)`
+//   width: 18px;
+//   height: 18px;
+// `;
