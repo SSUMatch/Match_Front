7e2736f5f83419e600f6e79fd72c97804df2febf
onejuneee <onejuneee721@gmail.com>
2024-04-28 05:37:57 +0900

    feat: RecoilRoot 추가

--- Match/src/main.tsx
+++ Match/src/main.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import GlobalStyle from "./styles/GlobalStyle";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {RecoilRoot} from 'recoil';
+import App from './App.tsx';
+import './index.css';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <RecoilRoot>
+      <GlobalStyle />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 );
