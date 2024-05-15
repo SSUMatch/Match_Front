c58ba75e32b01df3b49928859dda861c4e06b525
onejuneee <onejuneee721@gmail.com>
2024-05-16 08:58:19 +0900

    fix : 배포 전 storybook 삭제

--- Match/src/recoil/match/States.tsx
+++ Match/src/recoil/match/States.tsx
@@ -1,6 +1,42 @@
 import {atom} from 'recoil';
 import * as T from './Types';
 
+export const FavState = atom<T.FavTypes[]>({
+  key: 'FavState',
+  default: [
+    {
+      favId: 1,
+      name: '서울 숭실대학교 풋살 구장 A',
+      place: '동작구 상도동 상도로 369',
+    },
+    {
+      favId: 2,
+      name: '서울 용산 더베이스 1구장 (레알 마드리드)',
+      place: '용산구 한강대로23길 55',
+    },
+    {
+      favId: 3,
+      name: '서울 강남 도곡에프씨 (1구장)',
+      place: '강남구 선릉로 207',
+    },
+    {
+      favId: 4,
+      name: '서울 강동 고덕풋살장 (1구장)',
+      place: '강동구 아리수로 185',
+    },
+    {
+      favId: 5,
+      name: '서울 대방 투터치 풋살장 A',
+      place: '강남구 선릉로 207',
+    },
+    {
+      favId: 6,
+      name: '서울 대방 투터치 풋살장 B',
+      place: '강남구 선릉로 207',
+    },
+  ],
+});
+
 export const MatchListState = atom<T.MatchListTypes[]>({
   key: 'MatchListState',
   default: [
