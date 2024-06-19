05327e15fca604f1cfafd3aedd8316fc1861d2bb
onejuneee <onejuneee721@gmail.com>
2024-06-20 05:04:01 +0900

    feat : react-query 루트 적용

--- Match/src/main.tsx
+++ Match/src/main.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RecoilRoot} from 'recoil';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import App from './App.tsx';
 import './index.css';
 import GlobalStyle from './styles/GlobalStyle';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <GlobalStyle />
-      <App />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
